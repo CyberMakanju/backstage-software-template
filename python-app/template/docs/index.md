e5c6d927fa11aeb3adbae6b36b223467e7852ef7
Olalekan Adeyemi <olalekan.adeyemi@enbridge.com>
2026-02-03 17:01:08 +0000

    python template

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,9 @@
+# Documents for  ${{values.app_name}}
+
+This application has two endpoints:
+-  '/api/v1/details'
+- '/api/vi/healthz'
+here you could expand on what each of these endpoints do.
+
+#  How to access the app?
+you can access the app by accessing this URL: '${{values.app_name}}-${{values.app.env}}.test.com/api/v1/details'
---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, human@@!!2026 goal!!@@@! <3',
+        'deployed_on': 'kubernetes',
+        'deployed_on': 'argocd'
+        'env': '${{values.app.env}}',
+        'app_name': ' ${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+	# Do an actual check here
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
